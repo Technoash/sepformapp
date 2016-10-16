7a8c7cdb51d79b95939340a1ad61e563a661eea2
Edward Huang <edwardhuang@EdwardmatoMacBook-Air.local>
2016-10-17 07:44:19 +1100

    Change database ip

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ console.log("Formie Server v2");
 console.log("WARNING: Concurrency and request validation not fully implemented");
 
 var port = 8000;
-var dbip = '172.17.0.2';
+var dbip = '127.0.0.1';
 
 var Promise = require("bluebird");
 var express = require('express');
