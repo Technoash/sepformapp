cdd9ada7b746b623717b25729a3358dceba71f9a
Edward Huang <edwardhuang@EdwardmatoMacBook-Air.local>
2016-10-17 21:23:25 +1100

    Add account type when send request

--- static/site.js
+++ static/site.js
@@ -439,7 +439,7 @@ myApp.controller('CreateAccountController', function($scope, $request, $location
 			$scope.registerMessage('Email or password cannot be empty');
 			return;
 		}*/
-		$request.post('/auth/register', {email: $scope.email, password: $scope.password, firstName: $scope.firstName, lastName: $scope.lastName})
+		$request.post('/auth/register', {email: $scope.email, password: $scope.password, firstName: $scope.firstName, lastName: $scope.lastName, accountType: $scope.accountType})
 		.then(function(){
 			registerMessage("Registion completed!");
 			$window.location.reload();
